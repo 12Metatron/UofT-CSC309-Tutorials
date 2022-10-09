4988e6ccdd35048acdbe0b7bd3a0b15c2b16aa58
Zachary <zachary.kaufman@mail.utoronto.ca>
2022-10-09 16:21:45 -0400

    finished t1-t3

--- Week3-JS/main.js
+++ Week3-JS/main.js
@@ -4,53 +4,68 @@
     Then try to complete the line below to capture all div blocks that
     have the class "flex-item"
 */
-const items = 
+const items = document.querySelectorAll('div.flex-item');
+
+/*  querySelector("flex-item"),
+  querySelectorAll("flex-item"),
+  getElementsbyClassName("flex-item"),
+  getElementById("flex-item") */
 
 
-/* TODO: Complete the code below to find the minimum height of the <img> 
+/* TODO: Complete the code below to find the minimum height of the <img>
     element that exists in <div> blocks with "flex-item" class.
 */
-let min_img_height = Infinity; 
-for( ){
-  
+let min_img_height = Infinity;
+for(let item of items){
+  image = item.querySelector('img');
+  min_img_height = Math.min(image.height, min_img_height);
 }
+console.log(items);
 
-/* TODO: We want to add some functionality to the "Adjust!" button. When 
+/* TODO: We want to add some functionality to the "Adjust!" button. When
     a user clicks on the "Adjust!" button, it sets the height of every <img>
-    within the <div> block with "flex-item" class to the minimum height you 
-    found in the previous step (in pixel, i.e. px). In addition, we want to 
-    have "margin-bottom: 0.1rem" style added to each <img> element as well. 
+    within the <div> block with "flex-item" class to the minimum height you
+    found in the previous step (in pixel, i.e. px). In addition, we want to
+    have "margin-bottom: 0.1rem" style added to each <img> element as well.
 */
-const adjust_btn = 
-adjust_btn.onclick = function(){
-
+const adjust_btn = document.querySelector('#adjust-btn');
+adjust_btn.onclick = function() {
+    for(item of items){
+      item.querySelector('img').style['height'] = min_img_height + "px";
+      item.querySelector('img').style['margin-bottom'] = 0.1 + "rem";
+    }
 };
 
-/* TODO: Let's iterate through items again. But now, let's add two event 
-    listeners to each <div> block with class "flex-item". When a mouse 
-    enters the <div> block, the colour of its border changes randomly. 
-    In addition, the font weight of the product name changes to bold. 
+/* TODO: Let's iterate through items again. But now, let's add two event
+    listeners to each <div> block with class "flex-item". When a mouse
+    enters the <div> block, the colour of its border changes randomly.
+    In addition, the font weight of the product name changes to bold.
     When a mouse leaves the <div> block, everything reverts to its normal state.
 */
 let colors = ['red', 'blue', 'cyan', 'green', 'orange', 'brown', 'purple'];
-for( ){
+//console.log(items);
+for(let item of items){
+  console.log(item);
   /* TODO: For each <div> block add a "mouseenter" event. When the mouse
-    enters the block, randomly pick a colour from the array above. You 
-    might use Math.floor(), Math.random() functions. Once you pick the 
-    colour, set the border colour using the selected colour. Make sure 
+    enters the block, randomly pick a colour from the array above. You
+    might use Math.floor(), Math.random() functions. Once you pick the
+    colour, set the border colour using the selected colour. Make sure
     the font weight of the product name changes to bold.
   */
   item.addEventListener("mouseenter", (event) => {
-      
-
+    let c = colors[Math.floor(Math.random() * (colors.length))];
+    console.log("mouseenter worked: "+ c + ", item: " + item.id);
+    item.style['border-color'] = c;
+    item.querySelector('li').style['font-weight'] = 'bold';
+    item.querySelector('li').style['color'] = c;
     });
-  
-  /* TODO: For each <div> block add a "mouseleave" event. When the mouse leaves 
-      the block, revert all changes (i.e. border colour and font weight) to default.
-  */  
-  item.addEventListener("mouseleave", (event) => {
 
+  /* TODO: For each <div> block add a "mouseleave" event. When the mouse leaves
+      the block, revert all changes (i.e. border colour and font weight) to default.
+  */
+  item.addEventListener("mouseleave", (event) => {
+    item.style['border-color'] = 'black';
+    item.querySelector('li').style['font-weight'] = 'normal';
+    item.querySelector('li').style['color'] = 'black';
   });
 }
-
-
--- Week3-JS/order.js
+++ Week3-JS/order.js
@@ -1,24 +1,35 @@
 
-/* TODO: We have a hidden information box, just before the submit button on 
-    the order.html page and we want to show the total costs of the items a user selects. 
+/* TODO: We have a hidden information box, just before the submit button on
+    the order.html page and we want to show the total costs of the items a user selects.
     Select the <div> block with "info-msg" class:
 */
-let info_box = 
+let info_box = document.querySelector("div.info-msg");
+console.log(info_box);
+
+/*  querySelector("flex-item"),
+  querySelectorAll("flex-item"),
+  getElementsbyClassName("flex-item"),
+  getElementById("flex-item") */
 
 /* TODO: Select all <input> elements withing the <div> block that has the class "item-selection"
 */
-const item_checkboxes = 
+const item_checkboxes = document.querySelectorAll("#item-selection input");
+console.log(item_checkboxes);
 
-/* TODO: We want to show a message like: "hey USER! The total cost is: $80." in the 
+/* TODO: We want to show a message like: "hey USER! The total cost is: $80." in the
     info_box element when a user selects some items. You need to calculate the total
-    cost based on the user's selections. Replace the USER in the message above with 
-    the name that the user has entered in the Name input field. If there is nothing 
+    cost based on the user's selections. Replace the USER in the message above with
+    the name that the user has entered in the Name input field. If there is nothing
     entered, change it to a blank (i.e. "")
 */
+
 let total_price = 0
+var user = "";
+// added message myself
+let message = "hey " + user + "! The total cost is: $" + total_price + ".";
 // You need to iterate through all checkboxes
-for ( ){
-  /* Define an event listener for each checkbox (when a user selects an item or 
+for (let this_checkbox of item_checkboxes){
+  /* Define an event listener for each checkbox (when a user selects an item or
     deselects it, the message should be updated with the new total cost). Steps:
       1. Define the event listener for each checkbox
       2. Set the display style of info-msg block to "block" (the display property
@@ -26,10 +37,28 @@ for ( ){
       3. Find the cost of the selected item
       4. Convert the cost to an integer and update the total costs
       5. Find the name of the user entered in the name input field
-      6. Update the message in the info-msg block with the updated total cost and 
+      6. Update the message in the info-msg block with the updated total cost and
           user's name. Something like: "hey USER! The total cost is: $80."
     */
   this_checkbox.onclick = function(){
-    
+    //total_price += this_checkbox.cost
+    console.log(this_checkbox);
+    info_box.style["display"] = "block";
+    let box_name = this_checkbox.id;
+    var price = document.querySelector("label[for='"+box_name+ "'] span[class='price']");
+    console.log(price);
+    price = price.textContent;
+    console.log(price);
+    user = document.getElementById("name").value;
+    if (this_checkbox.checked) {
+      total_price += parseFloat(price.slice(1));
+    } else {
+      total_price -= parseFloat(price.slice(1));
+    }
+    message = "hey " + user + "! The total cost is: $" + total_price + ".";
+    info_box.textContent = message;
+    console.log(message);
   }
+
+
 }
